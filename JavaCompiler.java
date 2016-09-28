db701e79f816e6ba29645609cb92f1ef549a2b32
Edwin Richbald A. Salinas <edsalinas7b@yahoo.com>
2016-09-29 07:35:34 +0800

    Fixes stuff

--- JavaCompiler.java
+++ JavaCompiler.java
@@ -9,35 +9,23 @@ import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 public class Regexmatch {
-
-    public static boolean isTrue(String str) {
-        return str.matches("[tT]rue");
-    }
-
-    public static boolean containsFalse(String str) {
-        return str.matches(".*false.*");
-    }
-
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, IOException {
-        System.out.println(isTrue("True"));
-        System.out.println(containsFalse("kushfdskjdgf"));
         Scanner sc = new Scanner(new FileReader("input.txt"));
 
         //reads the file "input.txt"
-        PrintWriter writer = new PrintWriter("Regexmatch.java", "UTF-8");
+        PrintWriter writer = new PrintWriter("Test.java", "UTF-8");
 
         while (sc.hasNext()) {
             String str = sc.nextLine();
             //replaces if to kung
-            str = str.replaceAll("if", "kung");
-            System.out.println(str);
+            str = str.replaceAll("kung", "if");
             writer.println(str);
         }
         Runtime rt = Runtime.getRuntime();
-        rt.exec("javac " + System.getProperty("user.dir") + "\\Regexmatch.java", null, new File("C:\\Program Files\\Java\\jdk1.8.0_71\\bin\\"));
+        rt.exec("javac " + System.getProperty("user.dir") + "\\Test.java", null, new File("C:\\Program Files\\Java\\jdk1.8.0_71\\bin\\"));
         writer.close();
 
-        String command = "javac.exe " + System.getProperty("user.dir") + "\\Regexmatch.java";
+        String command = "javac.exe " + System.getProperty("user.dir") + "\\Test.java";
         try {
             Process p = Runtime.getRuntime().exec(command);
         } catch (Exception e) {
